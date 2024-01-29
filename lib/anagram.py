7f2efa88637c5c9fe260f062af48bad5966dd217
Andrew <andrewstaveley14@gmail.com>
2024-01-28 21:55:49 -0500

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, origin):
+        self.origin = origin
+        print(f"Anagram Detector Initialized with {origin}")
+    def match(self, word_list):
+        final_word = []
+        self.word_list = word_list
+        origin_separate = list(self.origin)
+        for word in word_list:
+            if sorted(list(word)) == sorted(origin_separate):
+                final_word.append(word)
+            else:
+                pass
+        return final_word
